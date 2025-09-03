d9d7cfd563619a2c4ea42ab68b429ea11ce58846
Timurs Zascerinskis <timurszascerinskis@gmail.com>
2025-09-03 11:00:58 +0300

    no 1 lidz 10

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("Timurs");
+// Funkcija, kas izvada skaitļus no 1 līdz 10
+function izvaditSkaitlus() {
+    for (let i = 1; i <= 10; i++) {
+        console.log(i);
+    }
+}
+
+// Saistām funkciju ar pogu
+document.getElementById("showNumbers").onclick = izvaditSkaitlus;
